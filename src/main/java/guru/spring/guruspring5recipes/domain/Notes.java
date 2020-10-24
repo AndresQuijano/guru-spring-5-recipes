656be5065e3606a12940b65ad4cfa7dada3cc8ed
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-10-24 13:25:18 -0500

    Lecture 166 done. Soles a problem related to a circular reference caused by the project Lombok.

--- src/main/java/guru/spring/guruspring5recipes/domain/Notes.java
+++ src/main/java/guru/spring/guruspring5recipes/domain/Notes.java
@@ -1,10 +1,12 @@
 package guru.spring.guruspring5recipes.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
     @Id
