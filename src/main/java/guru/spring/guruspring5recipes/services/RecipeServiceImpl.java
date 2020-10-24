5c6e4129abe9c43b20ae9cb45c97847c2d5b3be8
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-10-24 12:56:29 -0500

    Lecture 163 done. Refactor some classes using Lambok.

--- src/main/java/guru/spring/guruspring5recipes/services/RecipeServiceImpl.java
+++ src/main/java/guru/spring/guruspring5recipes/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package guru.spring.guruspring5recipes.services;
 
 import guru.spring.guruspring5recipes.domain.Recipe;
 import guru.spring.guruspring5recipes.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService{
     private final RecipeRepository recipeRepository;
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService{
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm the RecipeServiceImpl");
+
         Set<Recipe> recipes = new HashSet<>();
 
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
