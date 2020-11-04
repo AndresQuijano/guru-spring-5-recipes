8aa37746b4253916d590edc8c5e43cf0fe03b52d
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-11-03 20:42:03 -0500

    Lecture 193 done. Added findById method and a new view to show recipes. This is the first whole path from view to model and back using Spring MVC.

--- src/test/java/guru/spring/guruspring5recipes/services/RecipeServiceImplTest.java
+++ src/test/java/guru/spring/guruspring5recipes/services/RecipeServiceImplTest.java
@@ -8,9 +8,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
@@ -20,23 +22,43 @@ public class RecipeServiceImplTest {
     @Mock
     RecipeRepository recipeRepository;
 
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+
         recipeService = new RecipeServiceImpl(recipeRepository);
     }
 
     @Test
-    public void getRecipes() {
+    public void getRecipeByIdTest() throws Exception {
         Recipe recipe = new Recipe();
-        Set<Recipe> recipesTest=new HashSet<>();
-        recipesTest.add(recipe);
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
 
-        when(recipeRepository.findAll()).thenReturn(recipesTest);
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
 
-        Set<Recipe> recipesReturn = recipeService.getRecipes();
+        Recipe recipeReturned = recipeService.findById(1L);
 
-        assertEquals(recipesReturn.size(),1);
-        verify(recipeRepository, times(1)).findAll();
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
     }
+
+    @Test
+    public void getRecipesTest() throws Exception {
+
+        Recipe recipe = new Recipe();
+        HashSet receipesData = new HashSet();
+        receipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(receipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
+    }
+
 }
